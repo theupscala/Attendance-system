f32b3fc81433d0b9c4c2fb050f01725d7fcd0cd6
theupscala <theupscala@gmail.com>
2026-07-10 12:25:58 +0530

    Remove CSP meta tag and clean up Routes
    Removed Content Security Policy meta tag and unused Routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,11 +26,7 @@ const ProtectedRoute = ({ children }) => {
 
 function App() {
   return (
-    <Router>
-        {/* Content Security Policy to prevent XSS and malware attacks */}
-        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: http://localhost:5000; connect-src 'self' ws: http://localhost:5000; frame-src 'self' https://maps.google.com;" />
-     
-      <Routes>
+   
         <Route path="/login" element={<Login />} />
         
         <Route path="/" element={
