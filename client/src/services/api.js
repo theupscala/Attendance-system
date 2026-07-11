37e372dc52e3e7bd0726538540b6227ecb731559
Sanjay <sanjaygac@gmail.com>
2026-07-11 15:37:58 +0530

    Update backend API URL for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://rttdwo6s5npu5ielursfnvqw.187.127.182.214.sslip.io/api', // Changed to local backend
+  baseURL: 'http://rttdwo6s5npu5ielursfnvqw.187.127.182.214.sslip.io/api', // Deployed backend
   headers: {
     'Content-Type': 'application/json',
   }
